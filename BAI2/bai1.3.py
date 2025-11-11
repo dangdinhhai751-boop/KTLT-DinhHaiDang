927d39affdd8b63c492ac6b3b832d510f012d8ee
dangdinhhai751-boop <dangdinhhai751@gmail.com>
2025-11-11 16:44:17 +0700

    Delete BAI2/bai1.3.py

--- BAI2/bai1.3.py
+++
@@ -1,9 +0,0 @@
-print("Ho Va Ten:Dinh Hai Dang")
-print("MSSV:245752021610046")
-print("-----------")
-n = int(input("Enter a number: "))
-
-if n % 2 == 0:
-    print("EVEN Number")
-else:
-    print("ODD Number")
