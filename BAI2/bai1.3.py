02f73dcdede7dd09d0e9046d9714c87f172deb3c
dangdinhhai751-boop <dangdinhhai751@gmail.com>
2025-11-11 13:41:56 +0700

    Add files via upload

---
+++ BAI2/bai1.3.py
@@ -0,0 +1,9 @@
+print("Ho Va Ten:Dinh Hai Dang")
+print("MSSV:245752021610046")
+print("-----------")
+n = int(input("Enter a number: "))
+
+if n % 2 == 0:
+    print("EVEN Number")
+else:
+    print("ODD Number")
