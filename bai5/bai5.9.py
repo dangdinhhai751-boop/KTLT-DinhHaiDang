85b19369aafab7dddf0e62b8ec733e93fc2175b5
dangdinhhai751-boop <dangdinhhai751@gmail.com>
2025-12-08 10:36:43 +0700

    Update bai5.9.py

--- bai5/bai5.9.py
+++ bai5/bai5.9.py
@@ -1,5 +1,28 @@
 print("Ho va ten :Dinh Hai Dang")
 print("MSSV:245752021610046")
 print("---------------------")
+def binary_search(list, value):
+    lowerbound = 0
+    upperbound = len(list) - 1
+
+    while lowerbound <= upperbound:
+        midpoint = (lowerbound + upperbound) // 2
+        
+        if list[midpoint] == value:
+            return True # Đã tìm thấy
+        elif list[midpoint] < value:
+            lowerbound = midpoint + 1 # Bỏ qua nửa đầu
+        else:
+            upperbound = midpoint - 1 # Bỏ qua nửa sau
+
+    return False # Không tìm thấy
+list = list(map(int, input("Nhập list (các số cách nhau bởi khoảng trắng): ").split()))
+list.sort()
+print("List sau khi sắp xếp:", list) # In list đã sắp xếp
+
+value = int(input("Nhập giá trị cần tìm: "))
+
+result = binary_search(list, value)
+print("Kết quả:", result)
 
 
