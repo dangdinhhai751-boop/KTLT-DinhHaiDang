13f48ccd73e171b3dd1742bbfa8a047fd3e1283d
dangdinhhai751-boop <dangdinhhai751@gmail.com>
2025-12-08 10:39:16 +0700

    Update bai5.8.py

--- bai5/bai5.8.py
+++ bai5/bai5.8.py
@@ -1,2 +1,27 @@
+print("Ho va ten :Dinh Hai Dang")
+print("MSSV:245752021610046")
+print("---------------------")
+def sequential_search(dlist, item):
+    pos = 0
+    found = False
+
+    while pos < len(dlist) and not found:
+        if dlist[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+
+    return found, pos if found else -1
+
+# Chương trình chính
+dlist = list(map(int, input("Nhập danh sách các số cách nhau bởi dấu cách: ").split()))
+item = int(input("Nhập phần tử cần tìm: "))
+
+found, pos = sequential_search(dlist, item)
+
+if found:
+    print("Tìm thấy item tại vị trí (pos)", pos)
+else:
+    print("Không tìm thấy item trong danh sách")
 
 
